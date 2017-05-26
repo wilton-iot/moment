f556ce7e8980d4d6ae2407a36d40d12d26f8550c
alex <alex@staticlibs.net>
2017-05-26 12:07:06 +0100

    wilton sanity test added

---
+++ wilton-sanity-test.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require(["moment", "assert"], function(moment, assert) {
+    var date = moment("2010-10-20 4:30", "YYYY-MM-DD HH:mm");
+    var str = date.format("YYYY-MM-DD_HH:mm:ss");
+    console.log("test: moment sanity");
+    assert.equal(str, "2010-10-20_04:30:00");
+});
